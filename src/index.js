9570a0b11b820d74164337c50b47c56cd7a6929c
Bruce Gordon <brc.gordon@gmail.com>
2020-11-05 21:45:13 -0700

    Restyle login view

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 import { getUsers, getRooms, getBookings } from './fetch-requests'
 import User from '../src/Classes/User';
 import Manager from '../src/Classes/Manager';
