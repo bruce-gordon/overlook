2fd09ba4c46fc15f587a9b052a5f864838a6c67c
Bruce Gordon <brc.gordon@gmail.com>
2020-11-07 21:02:58 -0700

    Create function for login

--- src/DOM-methods.js
+++ src/DOM-methods.js
@@ -1,5 +1,12 @@
 export const domMethods = {
   sayHi: function() {
     console.log("Hello!");
-  }
+  },
+
+  // login:  function(userNameID, password) {
+  //   let ids = userData.map(user => {
+  //     return user.id;
+  //   })
+  //   if (ids.includes(userNameID) && password === 'overlook2020')
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,62 @@
 import './css/base.scss';
-import { getUsers, getRooms, getBookings } from './fetch-requests';
+
 import User from '../src/Classes/User';
 import Manager from '../src/Classes/Manager';
 import BookingRepo from '../src/Classes/Booking-Repo';
+
+import { getUsers, getRooms, getBookings, postBooking } from './fetch-requests';
 import { domMethods } from './DOM-methods';
-import { homeButton } from './DOM-variables';
+import {
+  homeButton,
+  roomSearchBar,
+  roomTypeInput,
+  roomDateInput,
+  searchRoomsButton,
+  loginView,
+  userName,
+  password,
+  loginButton,
+  loginError,
+  customerDash,
+  customerCharges,
+  customerBookings,
+  managerDash,
+  userSearch,
+  searchCustomerInput,
+  searchCustomerButton,
+  totalRevenue,
+  percentOccupied,
+  roomsVacant,
+  resultsHeading,
+  searchResult,
+  backButton
+} from './DOM-variables';
+
 let userData;
 let roomsData;
 let bookingsData;
 let user;
 
+loginButton.addEventListener("click", () => {
+  login(userName.value, password.value);
+})
 
+const login = (userName, password) => {
+  let userID = parseInt(userName.slice(8));
+  let allIds = userData.map(user => {
+    return user.id;
+  })
+  if (allIds.includes(userID) && password === 'overlook2020') {
+    user = new User(userData[userID - 1], bookingsData, roomsData);
+  }
+
+}
 
 const updateUserData = () => {
   getUsers()
   .then((data) => {
     userData = data.users;
+    console.log(userData);
   });
 }
 
@@ -34,6 +75,10 @@ const updateBookingsData = () => {
     console.log(bookingsData)
   })
 }
+
+updateUserData();
+updateRoomsData();
+updateBookingsData();
 
 const makeBooking = (date, roomNumber) => {
   let bookingDetails = user.bookRoom(date, roomNumber);
